c50f09d3aeae56c6f6c049428e5e87fabec06348
Kondo Takeo <takeoverjp@gmail.com>
2021-09-07 23:46:43 +0900

    5.3. Method Syntax

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -4,15 +4,30 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, rect: &Rectangle) -> bool{
+        self.height >= rect.height && self.width >= rect.width
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
-    println!("The area of the rectangle i {} square pixels.", area(&rect1));
-    println!("rect1 = {:?}", rect1)
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45
+    };
+    println!("The area of the rectangle i {} square pixels.", rect1.area());
+    println!("rect1 = {:?}", rect1);
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
